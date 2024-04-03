3419dc9a26a8f62922dc0e7f84d351ea814d9ab8
Alan Pugachev <aszneesizzle@gmail.com>
2024-04-03 13:39:29 +0300

    init

---
+++ src/main/java/com/alanpugachev/shinydisco/ShinyDiscoApplication.java
@@ -0,0 +1,13 @@
+package com.alanpugachev.shinydisco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShinyDiscoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShinyDiscoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alanpugachev/shinydisco/ShinyDiscoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanpugachev.shinydisco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShinyDiscoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
